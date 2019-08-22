0d0221048a56686a98e7025a4b920c6d36a42ff2
Eugene Bikkinin <xnegxneg@gmail.com>
2019-08-22 17:50:34 +0300

    Change tabs to spaces. Remove redundant .ToArray()

--- src/MediatR.Extensions.Microsoft.DependencyInjection/Registration/BehaviorRegistrar.cs
+++ src/MediatR.Extensions.Microsoft.DependencyInjection/Registration/BehaviorRegistrar.cs
@@ -6,50 +6,47 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace MediatR.Registration
 {
-	public static class BehaviorRegistrar
-	{
-		public static void RegisterBehaviorForCovariantRequest(
-			Type assignableRequestType,
-			IServiceCollection services,
-			IEnumerable<Assembly> assembliesToScan)
-		{
-			var types = assembliesToScan.SelectMany(a => a.DefinedTypes).ToArray();
+    public static class BehaviorRegistrar
+    {
+        public static void RegisterBehaviorForCovariantRequest(
+            Type assignableRequestType,
+            IServiceCollection services,
+            IEnumerable<Assembly> assembliesToScan)
+        {
+            var types = assembliesToScan.SelectMany(a => a.DefinedTypes).ToArray();
 
-			var handlers = types.Where(t =>
-					t.GetInterface(typeof(IRequestHandler<,>)) != null)
-				.ToArray();
+            var handlers = types.Where(t => t.GetInterface(typeof(IRequestHandler<,>)) != null);
 
-			var requestResponseTypes = handlers.Select(h =>
-					h.GetInterface(typeof(IRequestHandler<,>)).GetGenericArguments()[0])
-				.Where(assignableRequestType.IsAssignableFrom)
-				.Select(request => (request, request.GetInterface(typeof(IRequest<>)).GetGenericArguments()[0]))
-				.ToArray();
+            var requestResponseTypes = handlers.Select(h =>
+                    h.GetInterface(typeof(IRequestHandler<,>)).GetGenericArguments()[0])
+                .Where(assignableRequestType.IsAssignableFrom)
+                .Select(request => (request, request.GetInterface(typeof(IRequest<>)).GetGenericArguments()[0]));
 
-			var behaviorTypes = types.Where(t =>
-				{
-					var @interface = t.GetInterface(typeof(IPipelineBehavior<,>));
-					return @interface?.GetGenericArguments()[0].IsAssignableFrom(assignableRequestType) ?? false;
-				})
-				.ToArray();
+            var behaviorTypes = types.Where(t =>
+                {
+                    var @interface = t.GetInterface(typeof(IPipelineBehavior<,>));
+                    return @interface?.GetGenericArguments()[0].IsAssignableFrom(assignableRequestType) ?? false;
+                })
+                .ToArray();
 
-			foreach (var (requestType, responseType) in requestResponseTypes)
-			{
-				var typeArgs = new[] {requestType, responseType};
-				var closedInterfaceType = typeof(IPipelineBehavior<,>).MakeGenericType(typeArgs);
+            foreach (var (requestType, responseType) in requestResponseTypes)
+            {
+                var typeArgs = new[] {requestType, responseType};
+                var closedInterfaceType = typeof(IPipelineBehavior<,>).MakeGenericType(typeArgs);
 
-				foreach (var behaviorType in behaviorTypes)
-				{
-					var closedImplementationType = behaviorType.MakeGenericType(responseType);
-					services.AddTransient(closedInterfaceType, closedImplementationType);
-				}
-			}
-		}
+                foreach (var behaviorType in behaviorTypes)
+                {
+                    var closedImplementationType = behaviorType.MakeGenericType(responseType);
+                    services.AddTransient(closedInterfaceType, closedImplementationType);
+                }
+            }
+        }
 
-		private static Type GetInterface(this Type pluggedType, Type interfaceType)
-		{
-			return pluggedType
-				.GetInterfaces()
-				.FirstOrDefault(i => i.IsGenericType && i.GetGenericTypeDefinition() == interfaceType);
-		}
-	}
+        private static Type GetInterface(this Type pluggedType, Type interfaceType)
+        {
+            return pluggedType
+                .GetInterfaces()
+                .FirstOrDefault(i => i.IsGenericType && i.GetGenericTypeDefinition() == interfaceType);
+        }
+    }
 }
